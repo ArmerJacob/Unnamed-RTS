bfa70eaed06e64ce08b6e76f30d076b2ea8bdcca
jake armer <jakearmer@hotmail.co.uk>
2022-02-23 20:34:24 +0000

    initial upload

---
+++ RTS/Assets/Scripts/EntityMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    class EntityMovement
+    {
+
+        public Vector3 MoveEntity(Vector3 target, Transform transform, float offSet, float speed)
+        {
+            if (target.x > transform.position.x + offSet)
+            {
+                transform.position = new Vector3(transform.position.x + speed, transform.position.y, transform.position.z);
+            }
+            else if (target.x < transform.position.x - offSet)
+            {
+                transform.position = new Vector3(transform.position.x - speed, transform.position.y, transform.position.z);
+            }
+
+            if (target.z > transform.position.z + offSet)
+            {
+                transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + speed);
+            }
+            else if (target.z < transform.position.z - offSet)
+            {
+                transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - speed);
+            }
+
+            return transform.position;
+        }
+
+    }
+}
